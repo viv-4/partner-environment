d2df954cdf1a5f5218c8c2953b4521c650c03a8f
Caspian Baska <email@caspian.computer>
2021-07-21 00:46:04 +1000

    feat(placeos): implement a simpler tool structure

---
+++ scripts/spinner.sh
@@ -0,0 +1,108 @@
+#! /usr/bin/env bash
+# MIT License
+# Copyright (c) 2021 Tasos Latsas
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Author: Tasos Latsas
+
+# spinner.sh
+#
+# Display an awesome 'spinner' while running your long shell commands
+#
+# Do *NOT* call _spinner function directly.
+# Use {start,stop}_spinner wrapper functions
+
+# usage:
+#   1. source this script in your's
+#   2. start the spinner:
+#       start_spinner [display-message-here]
+#   3. run your command
+#   4. stop the spinner:
+#       stop_spinner [your command's exit status]
+
+function _spinner() {
+    # $1 start/stop
+    #
+    # on start: $2 display message
+    # on stop : $2 process exit status
+    #           $3 spinner function pid (supplied from stop_spinner)
+
+    local on_success="Done"
+    local on_fail="Fail"
+    local green='\033[1;32m'
+    local red='\033[0;31m'
+    local nc='\033[0m'
+
+    case $1 in
+    start)
+        # calculate the column where spinner and status msg will be displayed
+        let column=50-${#2}
+        # display message and position the cursor in $column column
+        echo -ne ${2}
+        printf "%${column}s"
+
+        # start spinner
+        i=1
+        sp='▓▒░'
+
+        delay=${SPINNER_DELAY:-0.15}
+
+        while :; do
+            printf "\b${sp:i++%${#sp}:1}"
+            sleep $delay
+        done
+        ;;
+    stop)
+        if [[ -z ${3} ]]; then
+            echo "spinner is not running.."
+            exit 1
+        fi
+
+        kill $3 >/dev/null 2>&1
+
+        # inform the user uppon success or failure
+        echo -en "\b["
+        if [[ $2 -eq 0 ]]; then
+            echo -en "${green}${on_success}${nc}"
+        else
+            echo -en "${red}${on_fail}${nc}"
+        fi
+        echo -e "]"
+        ;;
+    *)
+        echo "invalid argument, try {start/stop}"
+        exit 1
+        ;;
+    esac
+}
+
+function start_spinner {
+    # $1 : msg to display
+    _spinner "start" "${1}" &
+    # set global spinner pid
+    _sp_pid=$!
+    disown
+}
+
+function stop_spinner {
+    # $1 : command exit status
+    _spinner "stop" $1 $_sp_pid
+    unset _sp_pid
+}
